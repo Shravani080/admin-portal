2f956a2607bc77ee3204752086e7c0e45fc7b632
shravani <kshravanigoud08@gmail.com>
2023-02-28 10:46:24 +0100

    changes in table component

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -11,7 +11,8 @@ export class TableComponent {
 
   headers: any = ['Id', 'Name', 'Age', 'Gender'];
   newRow: any = { Name: '', Age: null, Gender: '' };
-  editRow: any = null;
+  editRow: any = "";
+  isEditing: boolean = false;
 
   rows: any = [
     { Id: 1, Name: 'Shravni', Age: 22, Gender: 'female' },
@@ -25,22 +26,33 @@ export class TableComponent {
     this.rows.splice(index, 1);
   }
 
-  onAddRow() {
-    const Id = this.rows.length + 1;
-    const newRow = {
-      Id: Id,
-      Name: this.newRow.Name,
-      Age: this.newRow.Age,
-      Gender: this.newRow.Gender,
-    };
-    this.rows.push(newRow);
-    this.newRow = { Name: '', Age: null, Gender: '' };
-  }
   onEditRow(row: any) {
-    this.editRow = row;
-    this.newRow.Name = row.Name;
-    this.newRow.Age = row.Age;
-    this.newRow.Gender = row.Gender;
+    this.editRow={...row};
+    this.newRow = { ...row };
+    this.isEditing = true;
+  }
+  
+  onAddOrUpdateRow() { 
+    if (this.isEditing) {
+      const index = this.rows.findIndex((row: any) => row.Id === this.editRow.Id);
+      if (index !== -1) {
+        this.rows[index].Name = this.newRow.Name;
+        this.rows[index].Age = this.newRow.Age;
+        this.rows[index].Gender = this.newRow.Gender;
+      }
+      this.editRow = "";
+      this.isEditing = false;
+    } else {
+      const Id = this.rows.length + 1;
+      const newRow = {
+        Id: Id,
+        Name: this.newRow.Name,
+        Age: this.newRow.Age,
+        Gender: this.newRow.Gender,
+      };
+      this.rows.push(newRow);
+      this.newRow = { Name: '', Age: null, Gender: '' };
+    }
   }
 
 }
